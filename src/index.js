5c676aeea7c4ebb5447972eaa94ec81efecba5e9
foisonocean <foisonocean@gmail.com>
2017-07-27 02:29:27 +0800

    :sparkles: Add CLI parser and improve exit behavior

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
+const yargs = require('yargs')
+
 const getCourseInfo = require('./course-info-analyzer')
 const getDownloadUrl = require('./download-analyzer')
 const download = require('./downloader')
 
-const lessonUrl = 'https://egghead.io/lessons/abc'
+const argv = yargs
+  .locale('en')
+  .usage('Usage: $0 [COMMAND] [--OPTIONS] URL')
+  .example('$0 \'https://egghead.io/lessons/abc\'')
+  .alias('h', 'help')
+  .help()
+  .alias('v', 'version')
+  .version()
+  .argv
+
+if (argv._.length > 1) {
+  console.error('\x1b[43m\x1b[41m%s\x1b[0m', 'Can only hava one url.')
+  process.exit(1)
+} else if (argv._.length === 0) {
+  console.error('\x1b[43m\x1b[41m%s\x1b[0m', 'Must have one url.')
+  process.exit(1)
+}
+
+const lessonUrl = argv._[0]
+
+if (!/^(http:\/\/|https:\/\/)?egghead\.io\/lessons\/.+$/ig.test(lessonUrl)) {
+  console.error('\x1b[43m\x1b[41m%s\x1b[0m', 'The url is invalid.')
+  process.exit(1)
+}
 
 ;(async function () {
+  console.log('Fetching the course info...')
   const listOfCourseInfo = await getCourseInfo(lessonUrl)
 
-  console.log('Course analyze complete.')
-  console.log(listOfCourseInfo.map(info => info.title).join('\n'))
-  console.log(`Total ${listOfCourseInfo.length} lessons`)
+  console.log('\nCourse analyze complete:')
+  console.log('\x1b[36m%s\x1b[0m', listOfCourseInfo.map(info => info.title).join('\n'))
+  console.log('\x1b[35m%s\x1b[0m', `Total ${listOfCourseInfo.length} lessons`)
 
   for (let i = 0; i < listOfCourseInfo.length; ++i) {
     const info = listOfCourseInfo[i]
@@ -22,4 +48,5 @@ const lessonUrl = 'https://egghead.io/lessons/abc'
     console.log(`Download complete: \`${name}\``)
   }
   console.log('👍 All completed.')
+  process.exit(0)
 })()

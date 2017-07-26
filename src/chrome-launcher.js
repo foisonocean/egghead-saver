50584c3516ba163001f5a9a1fa1958bcb7905c84
foisonocean <foisonocean@gmail.com>
2017-07-26 19:05:34 +0800

    :zap: Use single chrome process

--- src/chrome-launcher.js
+++ src/chrome-launcher.js
@@ -1,23 +1,44 @@
 const launcher = require('chrome-launcher')
 const CDP = require('chrome-remote-interface')
 
+let chrome = null
+
+async function getChrome () {
+  if (chrome == null) {
+    chrome = await launcher.launch({
+      chromeFlags: ['--headless', '--disable-gpu']
+    })
+  }
+  return chrome
+}
+
 async function chromeLauncher () {
-  const chrome = await launcher.launch({
-    chromeFlags: ['--headless', '--disable-gpu']
-  })
+  const chrome = await getChrome()
   const { port } = chrome
 
-  return new Promise(resolve => {
-    CDP(
+  return new Promise((resolve, reject) => {
+    CDP.New(
       { port },
-      async client => {
-        const { Network, Page } = client
-        await Promise.all([
-          Network.enable(),
-          Page.enable()
-        ])
+      (err, target) => {
+        if (err) {
+          reject(err)
+        } else {
+          CDP(
+            {
+              port,
+              target
+            },
+            async client => {
+              const { Network, Page } = client
+              await Promise.all([
+                Network.enable(),
+                Page.enable()
+              ])
 
-        resolve(client)
+              resolve(client)
+            }
+          )
+        }
       }
     )
   })
